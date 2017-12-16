d87b5f8ba7db16792f3fcae39511f875213d47a7
Stef <oacastefanita@gmail.com>
2017-12-16 19:26:57 +0200

    - push notifications - panorama on mac

---
+++ Example/RecorderFramework-MacExample/PanoramaViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PanoramaViewController.swift
+//  RecorderFramework-MacExample
+//
+//  Created by Stefanita Oaca on 16/12/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
