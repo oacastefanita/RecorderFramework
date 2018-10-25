b2d6b4086593af35752c8ccfd02c1e65487b9bc7
Stef <oacastefanita@gmail.com>
2018-10-25 09:01:38 +0300

    osx tag list fix

--- Example/RecorderFramework-MacExample/Extensions.swift
+++ Example/RecorderFramework-MacExample/Extensions.swift
@@ -26,7 +26,7 @@ func iterateEnum<T: Hashable>(_: T.Type) -> AnyIterator<T> {
     var i = 0
     return AnyIterator {
         let next = withUnsafeBytes(of: &i) { $0.load(as: T.self) }
-        if next.hashValue != i { return nil }
+//        if next.hashValue != i { return nil }
         i += 1
         return next
     }
