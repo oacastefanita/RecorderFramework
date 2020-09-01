5019e1a6879c7a73d119fefa8c978b3da423f638
Marius Avram <marius@codapper.com>
2020-09-01 11:37:43 +0300

    - set color for local folder - increment version and integrate in RECCALL

--- RecorderFramework/Classes/Controllers/APIClient.swift
+++ RecorderFramework/Classes/Controllers/APIClient.swift
@@ -379,6 +379,7 @@ public class APIClient : NSObject {
                 }else {
                     recordFolder?.title = name as String
                 }
+                recordFolder?.color = color
                 if let value:NSNumber = data!["id"] as? NSNumber {
                     recordFolder?.id = value.stringValue
                     for action in ActionsSyncManager.sharedInstance.actions {
